a39cc2e32ceba3bbc411a229fee021513492ee23
anargasimov1 <anar.gasimov.89@gmail.com>
2024-10-05 22:57:35 +0400

    v1.0.2

--- src/screens/UserPage.js
+++ src/screens/UserPage.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Alert, Pressable, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react';
 import Panel from '../companents/Panel';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -10,34 +10,45 @@ import * as Location from 'expo-location';
 import FontAwesomeIcon6 from 'react-native-vector-icons/FontAwesome6';
 
 const UserPage = ({ route }) => {
-
-    const [loc, setLocation] = useState({});
-    const [errorMsg, setErrorMsg] = useState(null);
-
+    const [location, setLocation] = useState({});
+    const [current, setCurrent] = useState(false);
+    const [indicator, setIndicator] = useState(false)
 
     useEffect(() => {
         getLocation();
-    }, []);
+    }, [location])
+
+    useEffect(() => {
+        getUserData()
+    }, [])
 
 
-   const getLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-            setErrorMsg('Location permission denied');
-            return;
+    const getLocation = async () => {
+        setIndicator(true)
+        try {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                Alert.alert('sizin icazeni levğ eləmisiz')
+                return;
+            }
+            let coords = await Location.getCurrentPositionAsync({ enableHighAccuracy: true })
+            setLocation({
+                latitude: coords.coords.latitude,
+                longitude: coords.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            })
+
+        } catch (error) {
+            console.log(error)
         }
-        let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
-        setLocation({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-        })
+        setCurrent(true)
+        setIndicator(false)
     }
 
 
-    const { id } = route.params;
 
+    const { id } = route.params;
 
     const [user, setUser] = useState('')
 
@@ -49,18 +60,13 @@ const UserPage = ({ route }) => {
     }
 
     async function getUserData() {
-
-
         try {
             const docRef = doc(db, 'users', id);
             const docSnap = await getDoc(docRef);
 
             if (docSnap.exists()) {
-
-                setUser(docSnap.data())
-
+                setUser(docSnap.data());
             } else {
-
                 console.log('Belə bir sənəd mövcud deyil');
             }
         } catch (error) {
@@ -68,18 +74,19 @@ const UserPage = ({ route }) => {
         }
     }
 
-    useEffect(() => {
-        getUserData();
-    }, [loc])
+
 
     return (
         <>
 
             <View style={styles.contanier} >
             </View>
-            <Text style={styles.text}>
-                xoş gelmisen  {user.name}
-            </Text>
+            <View style={styles.headText}>
+
+                <Text style={styles.text}>
+                    Xoş gəlmisən  {user.name}
+                </Text>
+            </View>
 
 
             <Text style={styles.text}>
@@ -91,23 +98,35 @@ const UserPage = ({ route }) => {
             <Text style={styles.text}>
                 nomre: {user.phone}
             </Text>
+            {indicator ? <ActivityIndicator style={styles.indicator} size="large" color="blue" /> : null}
 
-            <MapView
-                style={styles.map}
-                region={loc}
-            >
-                {/* {<Marker coordinate={{ latitude: loc.latitude, longitude: loc.longitude }} title="My Marker" style={{ color: 'red' }} />} */}
-            </MapView>
+            {
+                current ?
+                    <MapView
+                        style={styles.map}
+                        region={location}
+                    >
+                        {<Marker coordinate={{ latitude: location.latitude, longitude: location.longitude }} title="My Marker" pinColor='red' />}
+                    </MapView> :
 
+                    <MapView
+                        style={styles.map}
+                        region={{
+                            latitude: 40.56,
+                            longitude: 49.126
+                        }}
+                    >
+                        {<Marker coordinate={{ latitude: 50.56, longitude: 49.123 }} title="My Marker" pinColor='blue' />}
+                    </MapView>
 
-
+            }
 
             <Pressable onPress={() => singUp()} style={styles.button}>
                 <Text style={styles.text}>
                     Çıxış
                 </Text>
             </Pressable>
-            <Pressable onPress={() => { getLocation() }} style={styles.buttonLocation}>
+            <Pressable onPress={() => { getLocation(), setIndicator(true) }} style={styles.buttonLocation}>
                 <FontAwesomeIcon6 style={styles.icon} name="location-crosshairs" />
             </Pressable>
 
@@ -126,9 +145,25 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 50
     },
+    headText: {
+        flexDirection: 'row',
+        gap: 20,
+    },
+    title: {
+        backgroundColor: 'grey',
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+    },
+
+    titletext: {
+
+    },
     text: {
         fontWeight: 'bold',
-        fontSize: 30
+        fontSize: 20,
+        fontStyle: 'italic',
+        color: '#9592a1',
     },
     button: {
         width: 200,
@@ -141,10 +176,22 @@ const styles = StyleSheet.create({
     },
     icon: {
         fontSize: 30,
-        color: 'blue'
+        color: '#9592a1'
     },
     buttonLocation: {
         marginTop: 50,
-        marginLeft: 50
+        marginLeft: 50,
+        position: 'absolute',
+        top: 350,
+        right: 50,
+    },
+    indicator: {
+        width: 380,
+        height: 300,
+        position: 'absolute',
+        zIndex: 25,
+        backgroundColor: 'grey',
+        opacity: 0.5,
+        top: 160
     }
 })
